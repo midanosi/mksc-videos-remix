4411a1ac683fcccc690771198565ad488393ed29
Michael Hutchings <mdhutchings94@gmail.com>
2024-05-11 16:02:06 +0100

    fix: only open standard files once

--- app/.server/refresh_standards.ts
+++ app/.server/refresh_standards.ts
@@ -1,8 +1,28 @@
 import { db } from "~/lib/db.server";
-import { getStandardsArray } from "~/lib/getStandardsArray.server";
+import path from "path";
+import { promises as fs } from "fs";
 import { standardTitles } from "~/lib/standardTitles";
 
+async function openStandardCSV(mode: string) {
+  const publicDirectory = path.join(process.cwd(), "public");
+  const filename =
+    mode === "nonzzmt"
+      ? "standards_nonzzmt.csv"
+      : mode === "zzmt"
+      ? "standards_zzmt.csv"
+      : "standards_sc.csv";
+  const standardsFile = path.join(publicDirectory, filename);
+
+  const csv = await fs.readFile(standardsFile, "utf8");
+  return csv;
+}
+
 const modesWithStandards = ["zzmt", "sc", "nonzzmt"];
+const standardFiles = {
+  nonzzmt: await openStandardCSV("nonzzmt"),
+  zzmt: await openStandardCSV("zzmt"),
+  sc: await openStandardCSV("sc"),
+};
 
 async function refresh_standards() {
   for (const mode of modesWithStandards) {
@@ -13,11 +33,11 @@ async function refresh_standards() {
 
     await Promise.all(
       vids.map(async (vid) => {
-        const standards = await getStandardsArray({
-          cid: vid.cid,
-          mode: mode,
-        });
-        const metStandardIndex = standards.findIndex(
+        const file = standardFiles[mode] ?? undefined;
+        if (!file) return console.error("No file found for mode", mode);
+        const lines = (file as string).split("\n");
+        const standardRow = lines[vid.cid].split(",").slice(1); // slice 1 to ignore the cid at the start
+        const metStandardIndex = standardRow.findIndex(
           (standard) => Number(vid.time) < Number(standard)
         );
         if (metStandardIndex !== -1) {
