4cc46e938f8e4ebd33e6bdcc2fb1ca04d5a7d579
Michael Hutchings <mdhutchings94@gmail.com>
2024-05-11 16:28:20 +0100

    feat: pretty links

--- app/routes/videos.tsx
+++ app/routes/videos.tsx
@@ -204,19 +204,13 @@ export default function Videos() {
                   scope="col"
                   className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
                 >
-                  Standard
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-                >
                   Date
                 </th>
                 <th
                   scope="col"
                   className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
                 >
-                  URL
+                  YT
                 </th>
                 {displayingDeadLinks ? (
                   <th
@@ -244,7 +238,16 @@ export default function Videos() {
                       style={{ fontVariantNumeric: "tabular-nums" }}
                       className="text-right px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     >
-                      {mkscvid.time ? formatTime(Number(mkscvid.time)) : ""}
+                      {mkscvid.time ? (
+                        <a
+                          href={`https://youtu.be/${mkscvid.link}`}
+                          className="hover:text-blue-600"
+                        >
+                          {formatTime(Number(mkscvid.time))}
+                        </a>
+                      ) : (
+                        ""
+                      )}
                     </td>
                     <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-white">
                       {mkscvid.player}
@@ -258,7 +261,31 @@ export default function Videos() {
                         : ""}
                     </td>
                     <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-white underline">
-                      <a href={`https://youtu.be/${mkscvid.link}`}>URL</a>
+                      <a href={`https://youtu.be/${mkscvid.link}`}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 28.57  20"
+                          focusable="false"
+                          className="brightness-90"
+                        >
+                          <svg
+                            viewBox="0 0 28.57 20"
+                            preserveAspectRatio="xMidYMid meet"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g>
+                              <path
+                                d="M27.9727 3.12324C27.6435 1.89323 26.6768 0.926623 25.4468 0.597366C23.2197 2.24288e-07 14.285 0 14.285 0C14.285 0 5.35042 2.24288e-07 3.12323 0.597366C1.89323 0.926623 0.926623 1.89323 0.597366 3.12324C2.24288e-07 5.35042 0 10 0 10C0 10 2.24288e-07 14.6496 0.597366 16.8768C0.926623 18.1068 1.89323 19.0734 3.12323 19.4026C5.35042 20 14.285 20 14.285 20C14.285 20 23.2197 20 25.4468 19.4026C26.6768 19.0734 27.6435 18.1068 27.9727 16.8768C28.5701 14.6496 28.5701 10 28.5701 10C28.5701 10 28.5677 5.35042 27.9727 3.12324Z"
+                                fill="#FF0000"
+                              ></path>
+                              <path
+                                d="M11.4253 14.2854L18.8477 10.0004L11.4253 5.71533V14.2854Z"
+                                fill="white"
+                              ></path>
+                            </g>
+                          </svg>
+                        </svg>
+                      </a>
                     </td>
                     {displayingDeadLinks ? (
                       <td className="px-6 py-4 text-lg text-gray-500 whitespace-nowrap dark:text-white">
