cc65a3226cf40bb38e0859d9c940c2306afdc86a
Michael Hutchings <mdhutchings94@gmail.com>
2024-05-13 20:37:29 +0100

    feat: refetch is_user_admin on login/logout

--- app/context/AdminContext.tsx
+++ app/context/AdminContext.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@clerk/remix";
 import { useFetcher } from "@remix-run/react";
 import React, { useEffect, useMemo, useState } from "react";
 
@@ -6,16 +7,16 @@ const AdminContext = React.createContext<{
 }>({ isAdmin: false });
 
 function AdminContextProvider({ children }: { children: React.ReactNode }) {
+  const { userId } = useAuth();
   const fetcher = useFetcher();
   useEffect(() => {
     fetcher.load("/is_user_admin");
-  }, []);
+  }, [userId]);
 
   const isAdmin = useMemo(
     () => fetcher.data?.isAdmin ?? false,
     [fetcher.data?.isAdmin]
   );
-  console.log(`isAdmin`, isAdmin);
 
   return (
     <AdminContext.Provider value={{ isAdmin }}>
